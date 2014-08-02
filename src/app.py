f38ceca954579e7403138d77a62e8d19b5a759da
Ben Huson <j.ben.huson@gmail.com>
2014-08-02 11:32:22 -0400

    step 4: simplifying with tornado's generator module, using @gen.coroutine

--- src/app.py
+++ src/app.py
@@ -9,25 +9,26 @@ Example tornado application, for OSS lightning talk
 
 """
 
-from tornado import ioloop, web
+from tornado import ioloop, web, gen
 from models import BlockyCrypto
 import json
 
 class HelloHandler(web.RequestHandler):
-
-    def _blocky_crypto_process_callback(self, result):
-        self.set_status(200) # set the response status when we get the result back
-        self.write(json.dumps(dict(hello=result.result()))) #write the json in the response body
-        self.finish() # finish the asynch call
-
-    @web.asynchronous
+    @web.asynchronous # this is an async call
+    @gen.coroutine # decorate this call as a coroutine for tornado
     def get(self):
+        result = None
         try:
-            BlockyCrypto().blocky_crypto_process("world", callback=self._blocky_crypto_process_callback) # call our new blocky crypto instance
+            result = yield gen.Task( # create a coroutine task
+                BlockyCrypto().blocky_crypto_process,# that runs this method
+                "world") # with this parameter
         except Exception as e:
             self.set_status(500)
             self.write("An Exception has occurred: {0}".format(e))
             self.finish()
+        self.set_status(200) # set the response status when we get the result back
+        self.write(json.dumps(dict(hello=result.result()))) #write the json in the response body
+        self.finish() # finish the asynch call
 
 application = web.Application([ # here is our url/handler mappings, application url routing
     (r"/hello/", HelloHandler), # main handler takes a url parm, and we are passing session to initialize
